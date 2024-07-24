ef97e996ae741389b2a5a24f18445b949b8a5a6f
soojinsong <162791828+soojinsong@users.noreply.github.com>
2024-07-25 00:24:08 +0900

    widget design
    마이 페이지: 미니3, 다이어리, 메모 디자인.

---
+++ src/components/myPage/Memo.jsx
@@ -0,0 +1,24 @@
+import './MyPage.css';
+
+
+export function Memo(){
+
+    return(
+        <div className="mypage_Memo">
+            <div className='mypage_title'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="26" fill="none"><rect width="11.458" height="13.542" x="5.208" y="8.833" fill="url(#a)" rx="2"/><path stroke="#40300E" strokeLinecap="round" strokeWidth="1.2" d="M8.334 15.083h6.25M8.334 11.958H12.5M8.334 18.208H12.5M19.792 14.042v2.333c0 2.828 0 4.243-.879 5.121-.878.879-2.293.879-5.121.879h-2.583c-2.829 0-4.243 0-5.122-.879-.878-.878-.878-2.293-.878-5.121v-6.75c0-2.828 0-4.243.878-5.121.879-.879 2.293-.879 5.122-.879H12.5M18.75 3.625v6.25M21.875 6.75h-6.25"/><defs><linearGradient id="a" x1="5.208" x2="16.667" y1="15.604" y2="15.604" gradientUnits="userSpaceOnUse"><stop stopColor="#FFE14F"/><stop offset=".514" stopColor="#FFE14F"/></linearGradient></defs></svg>
+            <p className="mypage_title">TODAY'S MEMO</p>
+            </div>
+            
+            <div className="mypage_Memo_box">
+                <textarea className='memoWriteBox' rows='8' >
+
+                </textarea>
+
+            </div>
+        </div>
+    )
+}
+
+
+
--- src/components/myPage/MyPage.jsx
+++ src/components/myPage/MyPage.jsx
@@ -2,37 +2,17 @@ import { Header } from "../Header"
 import { Footer } from "../Footer"
 import './MyPage.css'
 import { useState } from "react"
-
+import { Mini3 } from "./Mini3.jsx";
+import { Diary } from "./Diary.jsx";
+import { Memo } from "./Memo.jsx";
 
 
 export function MyPage(){
-    const [isDrugChecked, setIsDrugChecked] = useState(false);
-    const [isCheckChecked, setIsCheckChecked] = useState(false);
-    const [isCautionChecked, setIsCautionChecked] = useState(false);
+  const [isDrugChecked, setIsDrugChecked] = useState(false);
+  const [isCheckChecked, setIsCheckChecked] = useState(false);
+  const [isCautionChecked, setIsCautionChecked] = useState(false);
 
-
-    // 모달창 체크 박스 
-    const handleDrugCheckboxChange = () => {
-      setIsDrugChecked(!isDrugChecked);
-      
-      setIsCheckChecked(false);
-      setIsCautionChecked(false);
-    };
-    const handleCheckCheckboxChange = () => {
-      setIsCheckChecked(!isCheckChecked);
-
-      setIsDrugChecked(false);
-      setIsCautionChecked(false);
-    };
-    const handleCautionCheckboxChange = () => {
-      setIsCautionChecked(!isCautionChecked);
-
-      setIsDrugChecked(false);
-      setIsCheckChecked(false);
-    };
-
-
-
+  
 
     return(
       <div className="screen_main">
@@ -63,47 +43,15 @@ export function MyPage(){
                 </div>
                 <button id='scheduleBtn'>+</button>
               </div>
-              <div className='memoList'>  
-                <input type='checkbox' className="checkBoxV" id='drugBtn' checked={isDrugChecked} onChange={handleDrugCheckboxChange} ></input>
-                <label htmlFor='drugBtn' className="memo" id='drug'>
-                  <p>약 복용 시간</p>
-                  </label>
-                
-                <input type='checkbox' className="checkBoxV" id='checkBtn' checked={isCheckChecked} onChange={handleCheckCheckboxChange}></input>
-                <label htmlFor='checkBtn' className='memo' id='check'>
-                  <p>검진 결과</p>
-                
-                </label>
-                <input type='checkbox' className="checkBoxV" id='cautionBtn' checked={isCautionChecked} onChange={handleCautionCheckboxChange}></input>
-                <label htmlFor='cautionBtn' className='memo' id='caution'>
-                  <p>약 주의사항</p>
-                
-                </label>
-
-              </div>
+              
             </div>
 
-            <div className='myPage_Diary'>
-              <div className='mypage_title'>
-                <p>TODAY'S DIARY</p>
-              </div>
-              <div className="mypage_Diary_up">
-                <div className="mypage_Diary_Imoge">
-                  <div className="kibun">
-                    <p className="Diary_title">오늘의 기분은 어떤가요?</p>
-                  </div>
-                  <div className="condition">
-                    <p className="Diary_title">오늘의 컨디션은 어떤가요?</p>
-                  </div>
-                </div>
-                <div className='kamsa'>
-                  <p className="Diary_title">오늘 감사한 일이 있었나요?</p>
-                </div>
-              </div>
-              <div className="mypage_Diary_down">
-                <p className="Diary_title">오늘 하루 일과는 어땠나요?</p>
-              </div>
-            </div>
+            <Mini3 
+              isDrugChecked={isDrugChecked} isCheckChecked={isCheckChecked} isCautionChecked={isCautionChecked}
+              setIsDrugChecked={setIsDrugChecked} setIsCheckChecked={setIsCheckChecked} setIsCautionChecked={setIsCautionChecked}
+              />
+
+            <Diary />
 
             <div className="mypage_Photo">
               <p className="mypage_title">TODAY'S PHOTO</p> 
@@ -140,12 +88,7 @@ export function MyPage(){
               </div>
             </div>
 
-            <div className="mypage_Memo">
-              <p className="mypage_title">TODAY'S MEMO</p>
-              <div className="mypage_Memo_box">
-
-              </div>
-            </div>
+            <Memo />
 
 
 
